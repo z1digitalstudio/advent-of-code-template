4a7debf0ddbd1af9cb3ad10cc51269a6be7c3614
María Simó <holasoymariasimo@gmail.com>
2023-11-20 09:53:56 +0100

    chore: add comments for dev mode

--- src/template/js/index.js
+++ src/template/js/index.js
@@ -1,4 +1,27 @@
+/**
+ * Welcome to in Advent of code
+ * Use these comments to learn how to work on your solutions. You can later delete them and adjust this template to your liking at `/src/template/js`
+ *
+ * Go to https://adventofcode.com/<year>/day/<dayNum> (the link is at the README file in this same folder) and read the problem statement.
+ * The problem includes an input example that you can use to work on your solution. You can copy that input example and use it to check you're on the right track.
+ * The real input is much bigger and is downloaded automatically for you at input.txt.
+ *
+ * Create a input-example.txt and use the readFile function to read its contents.
+ * You can use the dev mode `pnpm dev <dayNum>` and log the invoking of your function at the bottom of this file.
+ *
+ * Then, once you've solved the problem you can use send mode `pnpm send <dayNum>`.
+ * The send mode gets your functions exported here (part1 and part2, DON'T CHANGE THE NAMES) and runs them automagically passing the real input to them.
+ * You will see the result in the console and be able to submit it to Advent of code.
+ *
+ * Do you have questions? Join the AoC channel in Slack!
+ *
+ * Happy coding!
+ */
+
 import { readFile } from "./utils.js";
+
+// You can use readFile this way
+// const exampleInput = readFile(puzzles/day01/example.txt)
 
 const parseInput = (rawInput) => rawInput;
 
@@ -13,3 +36,6 @@ export const part2 = (rawInput) => {
 
   return;
 };
+
+// You can use the dev mode adding a console log here
+// console.log(part1(exampleInput))
